cf4d263422054ce92738f65738152fc8a165655c
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-10-19 10:43:13 -0400

    Autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
+var gulp         = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    $            = require('gulp-load-plugins')();
+
 $.exec = require('child_process').exec;
 
+
+// Server
 gulp.task('serve', function() {
   $.connect.server({
     root: ['site'],
@@ -12,15 +16,25 @@ gulp.task('serve', function() {
   $.exec('open http://localhost:3000');
 });
 
+
+// Compiling
 gulp.task('sass', function() {
   gulp.src('site/styles/style.scss')
     .pipe($.sass())
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(gulp.dest('site/styles'));
 });
 
+
+// Watching
 gulp.task('watch', function() {
   gulp.watch('sass/**/*.scss', ['sass']);
   gulp.watch('site/styles/**/*.scss', ['sass']);
 });
 
+
+// Tasks
 gulp.task('default', ['sass', 'watch', 'serve']);
