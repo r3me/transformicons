9fe27de709f18a869d18286d65b64420f6798318
jfroffice <jfroffice@gmail.com>
2014-11-08 16:31:33 +0100

    Package Builder #3 : CSS & SCSS on the fly on the server side

--- site/js/builder.js
+++ site/js/builder.js
@@ -1,69 +1,58 @@
-(function(window, document, builder) {
+var builder = (function(document, undefined) {
+  'use strict'
 
-  builder.utils = {};
-
-  builder.utils.readFile = function(filename) {
-    var request = new XMLHttpRequest();
-    var url = 'https://cdn.rawgit.com/grayghostvisuals/transformicons/master/sass/transformicons/' + filename;
-    request.open('GET', url, false);
-    request.send(null);
-
-    return request.responseText;
-  };
-
-  builder.utils.readFiles = function(filenames) {
-    var concat = '';
-    filenames.forEach(function(filename) {
-      var file = builder.utils.readFile(filename);
-      concat += file + '\n\n';
+  function getQueryString(input) {
+    var tmp = [];
+    [].forEach.call(document.querySelectorAll(input), function(elm) {
+      elm.checked && tmp.push(elm.getAttribute('data-transform'));
     });
-    return concat;
-  };
+    return tmp.join('&');
+  }
 
-  builder.utils.filterChecked = function() {
-    var checked = [];
-    var length = builder.settings.checkboxes.length;
-    for (var i = 0; i < length; i++) {
-      if(builder.settings.checkboxes[i].checked) {
-        checked.push(builder.settings.checkboxes[i]);
+  function getContent(type, qs, cb) {
+    var request = new XMLHttpRequest(),
+        url = window.location.origin + '/build/' + type + '?' + qs;
+
+    request.open('GET', url, false);
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400){
+        cb && cb(request.responseText);
+      } else {
+        console.warn('unknown error while reaching ' + url);
       }
+    };
+    request.send();
+  }
+
+  function getStylesheet(name) {
+    var type = 'css';
+    [].forEach.call(document.querySelectorAll('input[name="' + name + '"]'), function(elm) {
+      if (elm.checked) {
+        type = elm.value;
+      }
+    });
+    return type;
+  }
+
+  function buildStyles(input, type, cb) {
+    var queryString = getQueryString(input);
+    if (!queryString.length) {
+      cb && cb({ err: 'nothing to query'});
+      return;
     }
 
-    return checked;
-  };
-
-  builder.getAccordingFiles = function(checkbox) {
-    var files = checkbox.getAttribute('data-files').split(' ');
-
-    return files;
-  };
-
-  builder.getNeedles = function() {
-    var surelyNeeded = ['base/_config-globals.scss', 'base/_global-styles.scss'];
-    var checkedCheckboxes = builder.utils.filterChecked(builder.settings.checkboxes);
-    var selectedNeedles = [];
-    checkedCheckboxes.forEach(function(checkbox) {
-      selectedNeedles = selectedNeedles.concat(builder.getAccordingFiles(checkbox));
+    getContent(getStylesheet(type), queryString, function(data) {
+      cb && cb(null, data);
     });
-    var needles = surelyNeeded.concat(selectedNeedles);
+  }
 
-    return needles;
-  };
-
-  builder.attachEvents = function() {
-    builder.settings.form.addEventListener('submit', function(event) {
-      builder.generate();
-      event.preventDefault();
+  return function(options) {
+    document.querySelector(options.form).addEventListener('submit', function(e) {
+      buildStyles(options.input, options.type, function(err, data) {
+        document.querySelector(options.output).innerHTML = data || '';
+      })
+      e.preventDefault();
     });
-  };
+  }
 
-  builder.generate = function() {
-    builder.settings.textarea.value = builder.utils.readFiles( builder.getNeedles() );
-  };
-
-  builder.init = function(settings) {
-    builder.settings = settings;
-    builder.attachEvents();
-  };
-
-})(this, this.document, this.builder = {});
+})(this.document);
