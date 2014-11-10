7073c6faf238d9bafc107fbf5d0eee8321db5244
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-11-10 09:59:34 -0500

    Improves Tabs Scripting and Structure

--- site/js/src/tcontabs.js
+++ site/js/src/tcontabs.js
@@ -1,21 +1,27 @@
 define(['jquery'], function($) {
 
-  var $tcontabs_panel      = $('.tcon-tabs__group > div'),
-      $tcontabs_panel_one  = $('.tcon-tabs__group > div:first-of-type'),
-      $tcontabs_panel_last = $('.tcon-tabs__group > div:last-of-type'),
-      $tcon_style_radio    = $('input[name="tcon_stylesheet"]:radio'),
-      $tcon_js_radio       = $('input[name="tcon_javascript"]:radio'),
-      $tcon_css_tab        = $('#css-tab'),
-      $tcon_sass_tab       = $('#sass-tab'),
-      $tcon_jsmin_tab      = $('#jsmin-tab'),
-      $tcon_jsunmin_tab    = $('#jsunmin-tab'),
-      active_tab_class     = 'active';
+  // Config
+  // ===========================================================================
 
-  $tcontabs_panel.hide();
-  $tcontabs_panel_one.show();
+  var $tcontabs_panel           = $('.tcon-tabs__group > div'),
+      $tcontabs_panel_one       = $('.tcon-tabs__group > div:first-of-type'),
+      $tcontabs_panel_last      = $('.tcon-tabs__group > div:last-of-type'),
+      $tcon_style_radio         = $('input[name="tcon_stylesheet"]:radio'),
+      $tcon_js_radio            = $('input[name="tcon_javascript"]:radio'),
+      $tcon_tabs_tab            = $('.tcon-tabs__tab'),
+      $tcon_css_tab             = $('#css-tab'),
+      $tcon_sass_tab            = $('#sass-tab'),
+      $tcon_jsmin_tab           = $('#jsmin-tab'),
+      $tcon_jsunmin_tab         = $('#jsunmin-tab'),
+      active_tab_class          = 'active';
 
-  $tcon_style_radio.change(function() {
-    var input_style_val = $('input[name="tcon_stylesheet"]:checked').val();
+
+  // Methods
+  // ===========================================================================
+
+  function styleRadioChange() {
+    var $tcon_style_radio_checked = $('input[name="tcon_stylesheet"]:checked'),
+        input_style_val           = $tcon_style_radio_checked.val();
 
     if(input_style_val === 'css') {
       $tcontabs_panel.hide();
@@ -28,11 +34,12 @@ define(['jquery'], function($) {
       $tcon_css_tab.removeClass(active_tab_class);
       $tcon_sass_tab.addClass(active_tab_class);
     }
-  });
+  }
 
 
-  $tcon_js_radio.change(function() {
-    var input_js_val = $('input[name="tcon_javascript"]:checked').val();
+  function jsRadioChange() {
+    var $tcon_js_radio_checked    = $('input[name="tcon_javascript"]:checked'),
+        input_js_val              = $tcon_js_radio_checked.val();
 
     if(input_js_val === 'minified=true') {
       $tcontabs_panel.hide();
@@ -45,11 +52,11 @@ define(['jquery'], function($) {
       $tcon_jsmin_tab.removeClass(active_tab_class);
       $tcon_jsunmin_tab.addClass(active_tab_class);
     }
-  });
+  }
 
 
-  $('.tcon-tabs__nav a').on('click touchstart', function(e){
-    e.preventDefault();
+  function tconTabs(event) {
+    event.preventDefault();
 
     var $this    = $(this),
         tabgroup = '#'+$this.parents('.tcon-tabs__nav').data('tabgroup'),
@@ -60,5 +67,25 @@ define(['jquery'], function($) {
     $this.addClass(active_tab_class);
     $(tabgroup).children('div').hide();
     $(target).show();
+  }
+
+
+  // Events
+  // ===========================================================================
+
+  $tcontabs_panel.hide();
+  $tcontabs_panel_one.show();
+
+  $tcon_style_radio.on('change', function() {
+    styleRadioChange();
   });
+
+  $tcon_js_radio.on('change', function() {
+    jsRadioChange();
+  });
+
+  $tcon_tabs_tab.on('click touchstart', function(event) {
+    tconTabs.call(this, event);
+  });
+
 });
