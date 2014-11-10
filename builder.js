1c0faa02bb8d2ea3fc66801c37917d86d884591f
grayghostvisuals <grayghost@grayghostvisuals.com>
2014-11-09 19:43:47 -0500

    formatting

--- builder.js
+++ builder.js
@@ -14,20 +14,20 @@ var gulp                  = require('gulp'),
     BASE                  = '/build/',
     MSG_START             = 'WARN: "',
     MSG_END               = '" transform is not defined in deps.json',
-    contentMap            =  {
-                            'css': {
-                              build: buildCSS,
-                              mime : 'text/css'
-                            },
-                            'scss': {
-                              build: buildSCSS,
-                              mime: 'text/plain'
-                            },
-                            'js': {
-                              build: buildJS,
-                              mime: 'text/javascript'
-                            }
-                          };
+    contentMap            = {
+                              'css': {
+                                build: buildCSS,
+                                mime : 'text/css'
+                              },
+                              'scss': {
+                                build: buildSCSS,
+                                mime: 'text/plain'
+                              },
+                              'js': {
+                                build: buildJS,
+                                mime: 'text/javascript'
+                              }
+                            };
 
 // get files dependencies for all transformations
 // implicit include:
@@ -65,14 +65,15 @@ function getDependencies(params) {
   return _.uniq(files);
 }
 
+
 function getFiles(params) {
   return getDependencies(params).map(function(path) {
     return ROOT_SCSS + path;
   });
 }
 
-function buildStylesheet(isCSS, params, cb) {
 
+function buildStylesheet(isCSS, params, cb) {
   gulp.src(getFiles(params))
     .pipe(concat(CONCAT_FILE_IN_MEMORY))
     .pipe(isCSS ? sass() : gutil.noop())
@@ -85,22 +86,22 @@ function buildStylesheet(isCSS, params, cb) {
     }));
 }
 
-function buildSCSS(params, cb) {
 
+function buildSCSS(params, cb) {
   buildStylesheet(false, params, function(err, data) {
       cb && cb(err, data);
     });
 }
 
-function buildCSS(params, cb) {
 
+function buildCSS(params, cb) {
   buildStylesheet(true, params, function(err, data) {
     cb && cb(err, data);
   });
 }
 
-function buildJS(params, cb) {
 
+function buildJS(params, cb) {
   gulp.src(ROOT + FILENAME + '.js')
     .pipe(params.minified === 'true' ? uglify() : gutil.noop())
     .pipe(gutil.buffer(function(err, files) {
@@ -113,16 +114,18 @@ function buildJS(params, cb) {
     }));
 }
 
-exports.middleware = function() {
 
+exports.middleware = function() {
   return function(req, res, next) {
     var pathname = url.parse(req.url).pathname;
+
     if (pathname.indexOf(BASE) === -1) {
       next();
       return;
     }
 
     var type = pathname.substring(BASE.length, pathname.length);
+
     if (!type || !contentMap[type]) {
       next();
       return;
@@ -132,6 +135,7 @@ exports.middleware = function() {
 
       if (!err && buffer) {
         var contents = buffer[0]._contents;
+
         // TODO change file if minified
         res.setHeader('Content-Disposition', 'attachment; filename="' + FILENAME + '.' + type + '"');
         res.setHeader('Content-Type', contentMap[type].mime);
