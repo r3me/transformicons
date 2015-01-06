5660e2655cf181021eebf27b3626e26bb5abc48e
grayghostvisuals <grayghost@grayghostvisuals.com>
2015-01-06 17:45:09 -0500

    Heroku Server & Task Test

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var express = require("express")
 var app = express()
 var port = process.env.PORT || 5000
 
-app.use(express.static(__dirname + "/site"))
+app.use(express.static(__dirname + "/site/"))
 
 var server = http.createServer(app)
 server.listen(port)
