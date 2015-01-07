2a1f0b65508314eae307d3d9c276dd4a6f463d84
grayghostvisuals <grayghost@grayghostvisuals.com>
2015-01-06 20:49:44 -0500

    renaming tasks

--- gulpfile.js
+++ gulpfile.js
@@ -109,5 +109,5 @@ gulp.task('watch', function() {
 // Tasks
 // ===================================================
 
-gulp.task('build', ['sass', 'docs', 'watch', 'serve']);
-gulp.task('default', ['sass', 'docs']);
+gulp.task('default', ['sass', 'docs', 'watch', 'serve']);
+gulp.task('build', ['sass', 'docs']);
