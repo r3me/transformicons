ead0f3da60f7a8f053372cd7e7973fd5d95f7add
grayghostvisuals <grayghost@grayghostvisuals.com>
2015-01-07 11:06:10 -0500

    cleanup

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
-var WebSocketServer = require("ws").Server,
-    http            = require("http"),
-    express         = require("express"),
+var WebSocketServer = require('ws').Server,
+    http            = require('http'),
+    express         = require('express'),
     app             = express(),
     port            = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + "/site/"));
+app.use(express.static(__dirname + '/site/'));
 
 var server = http.createServer(app);
 server.listen(port);
@@ -13,13 +13,13 @@ console.log("http server listening on %d", port);
 var wss = new WebSocketServer({ server: server });
 console.log("websocket server created");
 
-wss.on("connection", function(ws) {
+wss.on('connection', function(ws) {
   var id = setInterval(function() {
     ws.send(JSON.stringify(new Date()), function() {});
   }, 1000);
   console.log("websocket connection open");
 
-  ws.on("close", function() {
+  ws.on('close', function() {
     console.log("websocket connection close");
     clearInterval(id);
   });
