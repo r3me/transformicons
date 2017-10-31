e9bd3ca637865cf15804563ff3510c7b44dc2905
dennisgaebel <dennisgaebel@gmail.com>
2017-10-31 14:05:57 -0400

    build configuration

--- gulpfile.js
+++ gulpfile.js
@@ -196,7 +196,7 @@ gulp.task('assemble', ['docs', 'copy'], function() {
 		.on('error', console.log)
 		.pipe(app.dest(paths.site))
 		.on('error', console.log)
-		.pipe($.livereload())
+		.pipe($.if(process.env.NODE_ENV === 'development', $.livereload()))
 
 	return stream;
 });
